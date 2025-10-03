e2c12a1720a85fddaad743a12aab550dfafe7d59
mukku <mukeshvermagbd20@gmail.com>
2025-10-03 09:47:22 +0000

    chore: format code with forge fmt

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -6,10 +6,11 @@ import "../src/TokenDistribution.sol";
 
 contract DeployToken is Script {
     function run() external {
-        
+
+
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
-        // MetaMask wallets 
+        // MetaMask wallets
         address wallet1 = 0x98D686f12cDe8B3006739EF50E2401E4bDC1d69A;
         address wallet2 = 0x321bF16523d5Ff1b0775677B8851938a64319577;
         address wallet3 = 0x11fB2A6dd15979a8421415019304968785cAE442;
@@ -17,8 +18,7 @@ contract DeployToken is Script {
         vm.startBroadcast(deployerKey);
 
         // Deploy token -  name, symbol, ToalSupply ,  wallets
-        new TokenDistribution(
-            "TokenDistribution", "TKD", 1_000_000, wallet1,wallet2, wallet3); // 1million tokens  , in wei 1e18
+        new TokenDistribution("TokenDistribution", "TKD", 1_000_000, wallet1, wallet2, wallet3); // 1million tokens  , in wei 1e18
 
         vm.stopBroadcast();
     }
--- src/TokenDistribution.sol
+++ src/TokenDistribution.sol
@@ -5,16 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenDistribution is ERC20, Ownable {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_,
+        address wallet1,
+        address wallet2,
+        address wallet3
+    ) ERC20(name_, symbol_) Ownable(msg.sender) {
+        uint256 amount = totalSupply_ * 1e18; // 1 million tokens
 
-
-    constructor( string memory name_ ,string memory symbol_ ,uint256 totalSupply_ ,address wallet1 ,address wallet2 ,address wallet3)
-     ERC20(name_, symbol_) 
-     Ownable(msg.sender) 
-    
-    {
-     uint256 amount = totalSupply_ * 1e18; // 1 million tokens
-    
-        // deployer is owner 
+        // deployer is owner
         _mint(msg.sender, amount);
 
         // distributing 20 % of 1 million to each wallet
@@ -27,7 +28,7 @@ contract TokenDistribution is ERC20, Ownable {
         // owner keeps 40%
     }
 
-    // Burn tokens 
+    // Burn tokens
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
