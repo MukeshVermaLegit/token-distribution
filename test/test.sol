d695fe2703e70b06b80a7a44b79c4f41337df2d4
mukku <mukeshvermagbd20@gmail.com>
2025-10-03 10:26:18 +0000

    fix: added a dummy test

---
+++ test/test.sol
@@ -0,0 +1,11 @@
+// test/Dummy.t.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+
+contract DummyTest is Test {
+    function testDummy() public pure {
+        assert(true);
+    }
+}
